036da7bcacb579e52ef6b0a81d39a7069630e87d
CanulJ <jesus34canul@gmail.comm>
2025-08-18 17:44:26 -0600

    Campos Vacaciones3

--- src/vacaciones/vacaciones.service.ts
+++ src/vacaciones/vacaciones.service.ts
@@ -1,3 +1,4 @@
+// vacaciones.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,24 +11,11 @@ export class VacacionesService {
     private readonly vacacionesRepository: Repository<Vacaciones>,
   ) {}
 
-  // Obtener todas las vacaciones
   findAll(): Promise<Vacaciones[]> {
-    return this.vacacionesRepository.find({
-      relations: ['usuario'], // si quieres traer también los datos del usuario
-    });
+    return this.vacacionesRepository.find({ relations: ['usuario'] });
   }
 
-  // Crear una nueva vacación
   create(vacacion: Vacaciones): Promise<Vacaciones> {
-    const nuevaVacacion = this.vacacionesRepository.create(vacacion);
-    return this.vacacionesRepository.save(nuevaVacacion);
-  }
-
-  // Opcional: buscar vacaciones de un usuario específico
-  findByUsuario(usuarioId: number): Promise<Vacaciones[]> {
-    return this.vacacionesRepository.find({
-      where: { usuarioId },
-      relations: ['usuario'],
-    });
+    return this.vacacionesRepository.save(vacacion);
   }
 }
