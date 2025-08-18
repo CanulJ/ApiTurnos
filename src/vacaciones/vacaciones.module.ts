6ed14a758b388499a56b88f57ea16ac0b78037f3
CanulJ <jesus34canul@gmail.comm>
2025-08-18 17:31:38 -0600

    Campos Vacaciones2

--- src/vacaciones/vacaciones.module.ts
+++ src/vacaciones/vacaciones.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VacacionesService } from './vacaciones.service';
 import { VacacionesController } from './vacaciones.controller';
+import { Vacaciones } from './vacaciones.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vacaciones])],
   providers: [VacacionesService],
   controllers: [VacacionesController],
 })
